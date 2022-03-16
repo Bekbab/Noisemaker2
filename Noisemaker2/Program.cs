0304507b0237c21d9f518c85a10af507673322de
Bekbab <72729623+Bekbab@users.noreply.github.com>
2022-03-16 14:03:06 +0100

    first commit

---
+++ Noisemaker2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Noisemaker2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
